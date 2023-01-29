7b95f38f8ef255dde3a823c5bb2016d73eab3bc7
daurenabasov <daurenabasov987@gmail.com>
2023-01-29 22:18:04 +0600

    create home page rout

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "../Pages/Home/HomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
